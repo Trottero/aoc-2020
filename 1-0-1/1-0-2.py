d72c81954a3141c072a25f3441fd5bd280b23a76
Trottero <wrlxs@hotmail.com>
2020-12-12 16:56:32 +0100

    🤮

--- 1-0-1/1-0-2.py
+++ 1-0-1/1-0-2.py
@@ -1,39 +1,29 @@
 import numpy as np
 
-file = open('C:/Users/Niels/source/repos/aoc-2020/1-0-1/input.txt').readlines()
+file = open('C:/Users/sava/source/repos/aoc-2020/1-0-1/input.txt').readlines()
 file = [int(l.strip()) for l in file]
 
 # Add in and outlets
 file.append(0)
-device_jolts = np.max(file) + 3
-file.append(device_jolts)
-
+file.append(np.max(file) + 3)
 file.sort()
-# All adapters have a -3 aswell
-
-jumps = []
-# Key: adapter from, value: amount of options
-adapter_dict = {}
-
-prev = 0
 
 
-def options(adapter, adapter_options):
-    r = []
-    for ad in adapter_options:
-        # for all of the options
-        if ad > adapter and ad <= adapter + 3:
-            r.append(ad)
-    return r
+# Start out jump list with the first # of options
+jumps = [1]
 
+for index, adapter in enumerate(file):
+    if index == 0:
+        continue
+    
+    chain_sum = 0
+    print('adapter:', adapter, file[max(0, index - 3):index])
+    for jndex, potadapter in enumerate(file[max(0, index - 3):index]): # Iterate through the previous 3 adapters
+        # check whether the current adapter connects to the previous adaptars
+        if potadapter + 3 >= adapter:
+            # Add the current known total to the current chain
+            chain_sum += jumps[max(0, index - 3) + jndex]
+    # Now that we know the total chain cost, we add it to the jump list
+    jumps.append(chain_sum)
 
-for index, adapter in enumerate(file[:-1]):
-    opts = options(adapter, file[index + 1:index + 4])
-    print('index:', index, 'adapter:', adapter, 'opts: ', opts, 'from: ', file[index + 1:index + 4])
-    adapter_dict[adapter] = len(opts)
-
-total = 1
-for opt_n in adapter_dict:
-    total *= adapter_dict[opt_n]
-
-print(total)
+print(jumps)
